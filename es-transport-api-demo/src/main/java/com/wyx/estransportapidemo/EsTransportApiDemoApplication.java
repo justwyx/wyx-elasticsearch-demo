8f8b82cab1dc6ac2c91069a2e40310815a2ac70a
yuxiang_wen <wenyvxiang@iiasaas.com>
2021-01-14 11:21:12 +0800

    add

---
+++ es-transport-api-demo/src/main/java/com/wyx/estransportapidemo/EsTransportApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.estransportapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTransportApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsTransportApiDemoApplication.class, args);
+	}
+
+}
